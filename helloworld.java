2115de86a4f4dcdcb1d412581d0adbf01f0e699b
hong-xiaohan <1694630480@qq.com>
2020-01-24 21:20:59 +0800

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+	public static void main(String arg[]){
+		System.out.println("helloworld");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+	}
+}
